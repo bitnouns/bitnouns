476b20e0a1bf08d8ef97a38b005cc86e98194dee
Milad Nekofar <milad@nekofar.com>
2023-11-26 17:30:45 +0330

    refactor: improve type safety in `MobileMenu` component
    Imported NavigationItem type from '@/types/ThemeConfig/NavigationItem' and Key from 'react' in MobileMenu.tsx for better type safety. Added explicit types in the .map() function for 'theme.nav.primary' to enhance code reliability and maintainability.

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,5 +1,7 @@
 import { useTheme } from '@/hooks/useTheme'
+import { NavigationItem } from '@/types/ThemeConfig/NavigationItem'
 import { Transition } from '@headlessui/react'
+import { Key } from 'react'
 import CustomConnectButton from './CustomConnectButton'
 import NavigationItemComponent from './NavigationItem'
 
@@ -17,13 +19,15 @@ export default function MobileMenu({ show }: { show: boolean }) {
       leaveTo="h-0 w-full"
     >
       <div className="mb-4 flex flex-col px-4">
-        {theme.nav.primary.map((item, i) => (
-          <NavigationItemComponent
-            key={i}
-            item={item}
-            className="mb-2 flex h-10 items-center justify-around rounded-xl border border-skin-stroke px-6 font-semibold text-skin-muted transition ease-in-out hover:scale-110"
-          />
-        ))}
+        {theme.nav.primary.map(
+          (item: NavigationItem, i: Key | null | undefined) => (
+            <NavigationItemComponent
+              key={i}
+              item={item}
+              className="mb-2 flex h-10 items-center justify-around rounded-xl border border-skin-stroke px-6 font-semibold text-skin-muted transition ease-in-out hover:scale-110"
+            />
+          ),
+        )}
         <CustomConnectButton className="h-10 w-full rounded-xl border border-skin-stroke bg-skin-backdrop px-6 text-skin-base transition ease-in-out hover:scale-110" />
       </div>
     </Transition>
