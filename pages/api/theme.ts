be4f2b4f09ab23a05797199c572e6411d06d5dbb
Milad Nekofar <milad@nekofar.com>
2023-11-26 10:25:27 +0330

    refactor: add support for Lodash in dynamic routes
    Extended runtime configuration for API routes to include unstable_allowDynamic option. This allows the use of Lodash, a popular utility library, in dynamic imports thereby increasing application flexibility and efficiency.

--- pages/api/theme.ts
+++ pages/api/theme.ts
@@ -3,6 +3,9 @@ import { theme } from "theme.config";
 
 export const config = {
   runtime: 'edge',
+  unstable_allowDynamic: [
+    '**/.pnpm/**/node_modules/lodash*/*.js',
+  ],
 }
 
 const handler = (_: NextApiRequest, res: NextApiResponse) => {
