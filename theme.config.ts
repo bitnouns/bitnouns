33180eb8831e12071301c70ab80013ba56546bf7
Milad Nekofar <milad@nekofar.com>
2023-11-26 15:13:45 +0330

    refactor: update import statements in `theme.config.ts`
    Changed relative import paths to alias-based paths in the file theme.config.ts. This modification improves the readability and maintainability of the code. This also aligns the import statements with the rest of the project using alias-based paths.

--- theme.config.ts
+++ theme.config.ts
@@ -1,6 +1,6 @@
-import merge from 'lodash.merge'
-import { lightTheme } from 'theme/default'
-import { ThemeConfig } from 'types/ThemeConfig'
+import { ThemeConfig } from "types/ThemeConfig";
+import { lightTheme } from "theme/default";
+import merge from "lodash.merge";
 
 export const theme: ThemeConfig = merge(lightTheme, {
   styles: {
