51434e817be62faa1343d96124a40870aba5e4a4
Milad Nekofar <milad@nekofar.com>
2023-11-26 17:22:46 +0330

    refactor: update `Footer` component for type safety
    Introduced explicit types for 'item' and 'i' in the .map() function over 'theme.nav.secondary' in Footer.tsx. Using explicit types, 'NavigationItem' for 'item' and 'Key | null | undefined' for 'i', enhances type checking and overall code safety. This code adjustment is crucial for reducing runtime errors and improving code maintainability.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import { useTheme } from '@/hooks/useTheme'
+import { NavigationItem } from '@/types/ThemeConfig/NavigationItem'
+import { Key } from 'react'
 import NavigationItemComponent from './NavigationItem'
 
 export default function Footer() {
@@ -7,13 +9,15 @@ export default function Footer() {
   return (
     <div className="flex justify-around py-16">
       <div className="flex flex-wrap items-center">
-        {theme.nav.secondary.map((item, i) => (
-          <NavigationItemComponent
-            key={i}
-            item={item}
-            className="sm:text-md mr-0 flex h-10 items-center justify-around rounded-xl px-4 text-sm text-skin-muted hover:text-skin-base sm:mr-4 sm:px-6"
-          />
-        ))}
+        {theme.nav.secondary.map(
+          (item: NavigationItem, i: Key | null | undefined) => (
+            <NavigationItemComponent
+              key={i}
+              item={item}
+              className="sm:text-md mr-0 flex h-10 items-center justify-around rounded-xl px-4 text-sm text-skin-muted hover:text-skin-base sm:mr-4 sm:px-6"
+            />
+          ),
+        )}
       </div>
     </div>
   )
